c36c1cfb69177882d68104806038c7040da9707c
Vinko <vinkoloic@gmail.com>
2024-07-02 03:02:37 +0200

    grab data of the modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -29,3 +29,57 @@ closeBtn.addEventListener("click", () => {
   modalbg.style.display = "none"
 })
   
+// recupération données formulaire
+let prenom = document.getElementById("first")
+prenom.addEventListener("change", ()=> {
+  console.log(prenom.value)
+})
+
+
+let nom = document.getElementById("last")
+nom.addEventListener("change", ()=> {
+  console.log(nom.value)
+})
+
+let email = document.getElementById("email")
+email.addEventListener("change", ()=> {
+  console.log(email.value)
+})
+
+
+let birthdate = document.getElementById("birthdate")
+birthdate.addEventListener("change", ()=> {
+  console.log(birthdate.value)
+})
+
+
+let quantity = document.getElementById("quantity")
+quantity.addEventListener("change", ()=> {
+  console.log(quantity.value)
+})
+
+
+let cguCheck = document.getElementById("checkbox1")
+console.log("CGU check = ", cguCheck.checked)
+cguCheck.addEventListener("change", ()=> {
+  console.log("CGU check = ", cguCheck.checked)
+})
+
+
+let newsLetter = document.getElementById("checkbox2")
+console.log("Newsletter check = ", newsLetter.checked)
+newsLetter.addEventListener("change", ()=> {
+  console.log("Newsletter check = ", newsLetter.checked)
+})
+
+
+let listeLocationBtn = document.querySelectorAll("input[type=radio]")
+for (let i=0; i<listeLocationBtn.length; i++) {
+  
+    if (listeLocationBtn[i].checked){
+      listeLocationBtn.addEventListener("change", () => {
+    console.log(listeLocationBtn[i].value)   
+})
+}
+}
+
