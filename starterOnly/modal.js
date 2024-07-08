39b943a877bfd0424547016496b13bdb93d17ff9
Vinko <vinkoloic@gmail.com>
2024-07-08 09:55:06 +0200

    tryhard save for empty field

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -32,6 +32,7 @@ closeBtn.addEventListener("click", () => {
 // recupération données formulaire
 let prenom = document.getElementById("first")
 prenom.addEventListener("change", ()=> {
+  
   console.log(prenom.value)
 })
 
@@ -73,13 +74,88 @@ newsLetter.addEventListener("change", ()=> {
 })
 
 
-let listeLocationBtn = document.querySelectorAll("input[type=radio]")
+let listeLocationBtn = document.querySelectorAll('input[name="citylocation"]')
+let citylocation = ""
 for (let i=0; i<listeLocationBtn.length; i++) {
-  
     if (listeLocationBtn[i].checked){
-      listeLocationBtn.addEventListener("change", () => {
-    console.log(listeLocationBtn[i].value)   
-})
+      citylocation = listeLocationBtn[i].value  
+      break }
+      listeLocationBtn[i].addEventListener("change", (event)=>{
+        console.log(event.target.value)
+      })
 }
-}
+
+
+// Message for wrong input
+const message = {
+  prenomOk: 'Minimum 2 caractères. Les chiffres et caractères spéciaux ne sont pas autorisés',
+  nameOk: 'Minimum 2 caractères. Les chiffres et caractères spéciaux ne sont pas autorisés',
+  emailOk: 'Veuillez renseigner une adresse mail valide.',
+  birthdateOk: 'Vous devez avoir plus de 18 ans pour participer',
+  quantityOk: 'Veuillez saisir une valeur numérique',
+  cityOk: 'Veuillez sélectionner une ville',
+  cguOk: `Vous devez accepter les conditions d'utilisation`,
+};
+
+const form = document.querySelector('form');
+
+let myError = document.querySelector("span");
+let myInput = document.querySelector("input");
+
+
+
+  let error = document.querySelectorAll("error")
+    
+
+
+
+
+// Quand on submit
+form.addEventListener("submit", (event) => {
+  // On empêche le comportement par défaut
+  event.preventDefault();
+  
+
+  if (!prenom.value) {
+    styleError()
+    error = "* Champ obligatoire *"
+    console.log(error)
+    
+  }
+
+  if (!nom.value) {
+    nom.appendChild(newSpan)
+    error = "** Champ obligatoire **"
+  }
+
+  if (!email.value) {
+    email.appendChild(newSpan)
+    error = "** Champ obligatoire **"
+  }
+
+  if (!birthdate.value) {
+    birthdate.appendChild(newSpan)
+    error = "** Champ obligatoire **"
+  }
+
+  if (!quantity.value) {
+    quantity.appendChild(newSpan)
+    error = "** Champ obligatoire **"
+  }
+
+
+
+// function check if form are ok
+function error()
+  if (!error) {
+    
+    myError.style.fontSize="0.8rem";
+    myError.style.color="red";
+    myInput.style.boxShadow= "-2px 2px 33px 4px rgba(255,0,0,0.67)"; 
+  }
+  
+  console.log("formulaire envoyé") 
+});
+
+
 
