cb40a128253c97d16779563ffa29c4c4c64b8769
Vinko <vinkoloic@gmail.com>
2024-07-08 13:47:25 +0200

    test2 error when empty and first name regexp

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -22,7 +22,7 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-// close Modal Event
+//** close Modal Event **//
 closeBtn.addEventListener("click", () => {
   // close modal form
   console.log("j'ai cliqué sur la croix")
@@ -30,57 +30,57 @@ closeBtn.addEventListener("click", () => {
 })
   
 // recupération données formulaire
-let prenom = document.getElementById("first")
-prenom.addEventListener("change", ()=> {
+let balisePrenom = document.getElementById("first")
+balisePrenom.addEventListener("change", ()=> {
   
-  console.log(prenom.value)
+  console.log(balisePrenom.value)
 })
 
 
-let nom = document.getElementById("last")
-nom.addEventListener("change", ()=> {
-  console.log(nom.value)
+let baliseNom = document.getElementById("last")
+baliseNom.addEventListener("change", ()=> {
+  console.log(baliseNom.value)
 })
 
-let email = document.getElementById("email")
-email.addEventListener("change", ()=> {
-  console.log(email.value)
-})
-
-
-let birthdate = document.getElementById("birthdate")
-birthdate.addEventListener("change", ()=> {
-  console.log(birthdate.value)
+let baliseEmail = document.getElementById("email")
+baliseEmail.addEventListener("change", ()=> {
+  console.log(baliseEmail.value)
 })
 
 
-let quantity = document.getElementById("quantity")
-quantity.addEventListener("change", ()=> {
-  console.log(quantity.value)
+let baliseBirthdate = document.getElementById("birthdate")
+baliseBirthdate.addEventListener("change", ()=> {
+  console.log(baliseBirthdate.value)
 })
 
 
-let cguCheck = document.getElementById("checkbox1")
-console.log("CGU check = ", cguCheck.checked)
-cguCheck.addEventListener("change", ()=> {
-  console.log("CGU check = ", cguCheck.checked)
+let baliseQuantity = document.getElementById("quantity")
+baliseQuantity.addEventListener("change", ()=> {
+  console.log(baliseQuantity.value)
 })
 
 
-let newsLetter = document.getElementById("checkbox2")
-console.log("Newsletter check = ", newsLetter.checked)
-newsLetter.addEventListener("change", ()=> {
-  console.log("Newsletter check = ", newsLetter.checked)
+let baliseCGUCheck = document.getElementById("checkbox1")
+console.log("CGU check = ", baliseCGUCheck.checked)
+baliseCGUCheck.addEventListener("change", ()=> {
+  console.log("CGU check = ", baliseCGUCheck.checked)
 })
 
 
-let listeLocationBtn = document.querySelectorAll('input[name="citylocation"]')
+let baliseNewsLetter = document.getElementById("checkbox2")
+console.log("Newsletter check = ", baliseNewsLetter.checked)
+baliseNewsLetter.addEventListener("change", ()=> {
+  console.log("Newsletter check = ", baliseNewsLetter.checked)
+})
+
+
+let baliseListeLocationBtn = document.querySelectorAll('input[name="citylocation"]')
 let citylocation = ""
-for (let i=0; i<listeLocationBtn.length; i++) {
-    if (listeLocationBtn[i].checked){
-      citylocation = listeLocationBtn[i].value  
+for (let i=0; i<baliseListeLocationBtn.length; i++) {
+    if (baliseListeLocationBtn[i].checked){
+      citylocation = baliseListeLocationBtn[i].value  
       break }
-      listeLocationBtn[i].addEventListener("change", (event)=>{
+      baliseListeLocationBtn[i].addEventListener("change", (event)=>{
         console.log(event.target.value)
       })
 }
@@ -88,74 +88,143 @@ for (let i=0; i<listeLocationBtn.length; i++) {
 
 // Message for wrong input
 const message = {
-  prenomOk: 'Minimum 2 caractères. Les chiffres et caractères spéciaux ne sont pas autorisés',
-  nameOk: 'Minimum 2 caractères. Les chiffres et caractères spéciaux ne sont pas autorisés',
-  emailOk: 'Veuillez renseigner une adresse mail valide.',
-  birthdateOk: 'Vous devez avoir plus de 18 ans pour participer',
-  quantityOk: 'Veuillez saisir une valeur numérique',
-  cityOk: 'Veuillez sélectionner une ville',
-  cguOk: `Vous devez accepter les conditions d'utilisation`,
+  balisePrenomOk: 'Minimum 2 caractères. Les chiffres et caractères spéciaux ne sont pas autorisés',
+  baliseNameOk: 'Minimum 2 caractères. Les chiffres et caractères spéciaux ne sont pas autorisés',
+  baliseEmailOk: 'Veuillez renseigner une adresse mail valide.',
+  baliseBirthdateOk: 'Vous devez avoir plus de 18 ans pour participer',
+  baliseQuantityOk: 'Veuillez saisir une valeur numérique',
+  baliseListeLocationBtnOk: 'Veuillez sélectionner une ville',
+  baliseCGUCheckOk: `Vous devez accepter les conditions d'utilisation`,
 };
 
+
+
+// let myError = document.querySelector("span");
+// let myInput = document.querySelector("input");
+
+
+
+
+//   if (!prenom.value) {
+//     styleError()
+//     error = "* Champ obligatoire *"
+//     console.log(error)
+    
+//   }
+
+//   if (!nom.value) {
+//     nom.appendChild(newSpan)
+//     error = "** Champ obligatoire **"
+//   }
+
+//   if (!email.value) {
+//     email.appendChild(newSpan)
+//     error = "** Champ obligatoire **"
+//   }
+
+//   if (!birthdate.value) {
+//     birthdate.appendChild(newSpan)
+//     error = "** Champ obligatoire **"
+//   }
+
+//   if (!quantity.value) {
+//     quantity.appendChild(newSpan)
+//     error = "** Champ obligatoire **"
+//   }
+
+
+
+// // function check if form are ok
+// function error()
+//   if (!error) {
+    
+//     myError.style.fontSize="0.8rem";
+//     myError.style.color="red";
+//     myInput.style.boxShadow= "-2px 2px 33px 4px rgba(255,0,0,0.67)"; 
+//   }
+  
+//   console.log("formulaire envoyé") 
+// });
+
+//** form declaration and test when submit **/
 const form = document.querySelector('form');
 
-let myError = document.querySelector("span");
-let myInput = document.querySelector("input");
+
+/*function to check empty field */
+function checkFields(balise) {
+  if (!balise.value) {
+    let error = document.getElementById('error')
+    balise.classList.add('input_error')
+    error.innerHTML="** Champ Obligatoire **"
+    console.log(balise.classList)
+  } else {
+    balise.classList.remove("input_error")
+  }
+}
+
+// function to check correct expression for mail
+function checkEmail(balise) {
+  let emailRegExp = new RegExp("^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$")
+  if (emailRegExp.test(balise.value)) {
+    balise.classList.remove("input_error")
+    } else {
+      balise.classList.add('input_error')
+      error="balise+Ok"
+    }
+}
 
 
 
-  let error = document.querySelectorAll("error")
-    
-
-
-
+// function to check correct expression for mail
+function checkAlpha(balise) {
+  let alphaRegExp = new RegExp("\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+")
+  if (alphaRegExp.test(balise.value)) {
+    balise.classList.remove("input_error")
+    } else {
+      balise.classList.add('input_error')
+      error="balise+Ok"
+    }
+}
 
 // Quand on submit
 form.addEventListener("submit", (event) => {
+  checkAlpha(balisePrenom)
+  checkFields(balisePrenom)
   // On empêche le comportement par défaut
   event.preventDefault();
-  
-
-  if (!prenom.value) {
-    styleError()
-    error = "* Champ obligatoire *"
-    console.log(error)
-    
-  }
-
-  if (!nom.value) {
-    nom.appendChild(newSpan)
-    error = "** Champ obligatoire **"
-  }
-
-  if (!email.value) {
-    email.appendChild(newSpan)
-    error = "** Champ obligatoire **"
-  }
-
-  if (!birthdate.value) {
-    birthdate.appendChild(newSpan)
-    error = "** Champ obligatoire **"
-  }
-
-  if (!quantity.value) {
-    quantity.appendChild(newSpan)
-    error = "** Champ obligatoire **"
-  }
-
-
-
-// function check if form are ok
-function error()
-  if (!error) {
-    
-    myError.style.fontSize="0.8rem";
-    myError.style.color="red";
-    myInput.style.boxShadow= "-2px 2px 33px 4px rgba(255,0,0,0.67)"; 
-  }
-  
-  console.log("formulaire envoyé") 
 });
 
+form.addEventListener("submit", (event) => {
+  // On empêche le comportement par défaut
+  event.preventDefault();
+  checkFields(baliseNom)
+});
 
+/*au changement tant que l'email n'est pas bien écrit*/
+baliseEmail.addEventListener("change", () => {
+  checkEmail(baliseEmail)
+})
 
+// form.addEventListener("submit", (event) => {
+//   // On empêche le comportement par défaut
+//   event.preventDefault();
+//   checkFields(baliseBirthdate)
+// });
+
+// form.addEventListener("submit", (event) => {
+//   // On empêche le comportement par défaut
+//   event.preventDefault();
+//   checkFields(baliseQuantity)
+// });
+
+// form.addEventListener("submit", (event) => {
+//   On empêche le comportement par défaut
+//   event.preventDefault();
+//   checkFields(baliseListeLocationBtn)
+// });
+
+form.addEventListener("submit", (event) => {
+  // On empêche le comportement par défaut
+  event.preventDefault();
+  checkFields(baliseCGUCheck)
+});
