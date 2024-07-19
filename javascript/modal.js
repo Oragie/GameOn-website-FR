3099e399440820a530d2fe837d736feb42345400
Vinko <vinkoloic@gmail.com>
2024-07-19 08:14:20 +0200

    add comments for futher user

--- javascript/modal.js
+++ javascript/modal.js
@@ -3,24 +3,24 @@ import {
   checkIfBirthdateValue,
   checkIfConditionsAccepted,
   checkIfCitySelected,
-} from "./helpers.js";
+} from "./helpers.js"; // Importation des fonctions de validation depuis helpers.js
 
-// Modal Navigation
+// Sélection des éléments de la modale
 const formWrapper = document.querySelector(".form_wrapper");
 const modalSuccess = document.querySelector(".modal_success");
 const btnSignup = document.querySelectorAll(".btn_signup");
 const modalClose = document.querySelector(".btn-close");
 const btnNav = document.querySelector("#btn_hamb");
 
-// Form
+// Sélection du formulaire
 const form = document.querySelector("form");
 
-// Toggle navbar
+// Ajout d'un écouteur d'événement pour basculer la navigation
 btnNav.addEventListener("click", () =>
   document.querySelector(".list").classList.toggle("menu_toggle")
 );
 
-// Open / Close Modal Form
+// Ouvrir/Fermer la modale de formulaire
 btnSignup.forEach((btn) => {
   btn.addEventListener("click", () => (formWrapper.style.display = "flex"));
 });
@@ -29,7 +29,7 @@ modalClose.addEventListener(
   () => (formWrapper.style.display = "none")
 );
 
-// Message error
+// Messages d'erreur spécifiques
 const message = {
   name: "Saisir minimum 2 caractères, ne peut être vide.",
   email: "Veuillez entrer une adresse mail valide, ne peut être vide.",
@@ -39,22 +39,25 @@ const message = {
   conditions: `Vous devez accepter les conditions général d'utilisation.`,
 };
 
-// Regex
+// Regex pour valider les entrées
 const regexName = /^([A-Za-z|\s]{2,15})?([-]{0,1})?([A-Za-z|\s]{2,15})$/;
 const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 const regexQuantity = /^([0-9]+)$/;
 
-// Validate form
+// Fonction de validation du formulaire
 function validate(event, type) {
   event.preventDefault();
   if (!type)
-    throw new Error("Type is required, choose between 'submit' or 'input'");
+    throw new Error("Type is required, choose between 'submit' or 'input'"); // Vérification du type d'événement
+
   let elements = null;
   if (type === "submit") {
-    elements = event.target.elements;
+    elements = event.target.elements; // Récupération des éléments du formulaire lors de la soumission
   } else {
-    elements = event.target.form.elements;
+    elements = event.target.form.elements; // Récupération des éléments du formulaire lors de l'entrée
   }
+
+  // Récupération des champs du formulaire
   const firstnameField = elements["first"];
   const lastnameField = elements["last"];
   const birthdateField = elements["birthdate"];
@@ -62,7 +65,8 @@ function validate(event, type) {
   const quantityField = elements["quantity"];
   const conditionsCheckbox = elements["checkbox1"];
   const allBtnRadio = elements["location"];
-  // Check if all conditions are valid
+
+  // Validation des différents champs
   const isConditionsAccepted = checkIfConditionsAccepted(
     conditionsCheckbox,
     message.conditions
@@ -89,7 +93,7 @@ function validate(event, type) {
     message.name
   );
 
-  // If all conditions are valid
+  // Si toutes les conditions sont remplies lors de la soumission
   if (
     type === "submit" &&
     isConditionsAccepted &&
@@ -100,16 +104,17 @@ function validate(event, type) {
     isLastNameValid &&
     isFirstNameValid
   ) {
-    formWrapper.style.display = "none";
-    modalSuccess.style.display = "flex";
-    form.reset();
+    formWrapper.style.display = "none"; // Fermeture de la modale de formulaire
+    modalSuccess.style.display = "flex"; // Affichage de la modale de succès
+    form.reset(); // Réinitialisation du formulaire
   }
 }
 
+// Ajout des écouteurs d'événements pour la soumission et l'entrée du formulaire
 form.addEventListener("submit", (event) => validate(event, "submit"));
 form.addEventListener("input", (event) => validate(event, "input"));
 
-// Close Success Modal
+// Fermeture de la modale de succès
 document
   .querySelector(".modal_content button")
   .addEventListener("click", () => (modalSuccess.style.display = "none"));
